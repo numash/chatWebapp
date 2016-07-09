ff930bd9fa7dbfe354936c3d913eb9610e7966ff
Малыщенко Маша <malishenko.ru@mail.ru>
2016-07-09 20:56:07 +0300

    socket connection established

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,38 +1,21 @@
 /**
  * Created by numash on 08.07.2016.
  */
+
 "use strict";
 
-var userName = "Lalka";
+$("#messageForm").submit(function( event ) {
+    event.preventDefault();
+    var message = event.currentTarget[0].value;
+    //console.log("Message to send: " + message);
 
+    $("#message").val("");
 
-function sendMessage(event) {
-    var message = event.currentTarget.form[0].value;
-    //alert(message);
-    var socket = new WebSocket("ws://localhost:8081");
-
-    socket.onopen = function() {
-        alert("Соединение установлено.");
-    };
-
-    socket.onclose = function(event) {
-        if (event.wasClean) {
-            alert('Соединение закрыто чисто');
-        } else {
-            alert('Обрыв соединения'); // например, "убит" процесс сервера
-        }
-        alert('Код: ' + event.code + ' причина: ' + event.reason);
-    };
-
-    socket.onmessage = function(event) {
-        alert("Получены данные " + event.data);
-    };
-
-    socket.onerror = function(error) {
-        alert("Ошибка " + error.message);
-    };
-
-    socket.send(message);
+    socket.emit('chat message', message);
     return false;
-}
+});
 
+    socket.on('chat message', function(msg){
+    console.log("Message received: " + msg);
+    $("#messagesField").text(msg);
+})
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,23 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
+var app = express();
+var server = app.listen(8080);
+var io = require('socket.io').listen(server);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', function(req, res){
+    res.render('index', { title: 'SS' });
 });
 
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
+});
 
+server.listen(app.get('port'), function(){
+    console.log('listening on *: ' + app.get('port'));
+});
 
-module.exports = router;
+module.exports = app;
